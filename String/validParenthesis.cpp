4163c7443e52c3bfe0c6efaef33f7b3dc5bbd255
panjyar <panjiyarkehuli@gmail.com>
2025-06-22 11:04:46 +0530

    Lt: checking valid parenthesis

---
+++ String/validParenthesis.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(int i = 0 ; i < s.length() ; i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[')   
+              st.push(s[i]);
+            else {
+                if(st.empty()) return false;
+                char top = st.top();
+                if((s[i] == ')' && top != '(') ||
+                   (s[i] == '}' && top != '{') ||
+                   (s[i] == ']' && top != '[')
+                ) return false;
+
+                st.pop();
+            }   
+        }
+        return st.empty();
+
+        
+    }
+};
+
+int main(){
+    string str = "{[)]}";
+    Solution sol;
+    cout<<"Valid Parenthesis :  "<<sol.isValid(str);
+    return false;
+}
